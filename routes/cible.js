2a6cc00d8e1908c0327733da2275c8a353fc2cab
hcasanov <noelledeur@gmail.com>
2020-02-17 11:17:53 +0100

    Developpement de l'algo de selection des profiles

--- routes/cible.js
+++ routes/cible.js
@@ -12,7 +12,7 @@ const pool = new pg.Pool(config);
 module.exports = {
     get: function (req, res) {
         pool.connect(function (err, client, done) {
-            if (err){
+            if (err) {
                 done()
                 res.status(500).send('Internal Server Error')
             }
@@ -20,22 +20,22 @@ module.exports = {
                 var id = jwt_decode(req.headers.token).id;
                 var query = "SELECT token FROM accounts WHERE id = \'" + id + "\';";
                 client.query(query, (err, result) => {
-                    if (err){
+                    if (err) {
                         done()
                         return res.status(500).send('Internal Server Error');
                     }
-                    else if (result == undefined){
+                    else if (result == undefined) {
                         done()
                         return res.status(401).send('Unauthorized');
                     }
                     else if (result.rows[0].token === req.headers.token) {
                         var query = "Select * FROM accounts WHERE id = " + id + ";"
                         client.query(query, (err, result) => {
-                            if (err){
+                            if (err) {
                                 done()
                                 res.status(500).send('Internal Server Error')
                             }
-                            else if (result.rows[0] == undefined){
+                            else if (result.rows[0] == undefined) {
                                 done()
                                 res.status(400).send('Bad Request')
                             }
@@ -45,12 +45,8 @@ module.exports = {
                                 var research_gender = result.rows[0].research_gender
                                 var gender = result.rows[0].gender
                                 var id = result.rows[0].id
-                                if(research_gender === 'M' || research_gender === 'F'){
-                                    var query = "SELECT * FROM accounts \
-                                            WHERE (age BETWEEN " + research_age_min + " AND " + research_age_max + ") \
-                                            AND gender = \'" + research_gender + "\' AND research_gender = \'" + gender + "\' \
-                                            AND blocked = false \
-                                            AND id != " + id + ";"
+                                if (research_gender === 'M' || research_gender === 'F') {
+                                    var query = "SELECT * FROM accounts LEFT JOIN likes ON accounts.id = likes.to_id WHERE (accounts.age BETWEEN " + research_age_min + " AND " + research_age_max + ") AND accounts.gender = \'" + research_gender + "\' AND accounts.research_gender = \'" + gender + "\' AND accounts.blocked = false;"
                                 }
                                 else {
                                     var query = "SELECT * FROM accounts \
@@ -60,13 +56,20 @@ module.exports = {
                                             AND id != " + id + ";"
                                 }
                                 client.query(query, (err, result) => {
-                                    if (err){
+                                    if (err) {
                                         done()
                                         res.status(500).send('Internal Server Error')
                                     }
                                     else {
                                         done()
-                                        
+                                        var tab = [];
+                                        console.log(result.rows)
+                                        // result.rows.forEach(el => {
+                                        //     console.log(el)
+                                        //     // if(!tab.find(user => user.id === el.id))
+                                        // })
+                                        // console.log(result.rows)
+                                        return res.json(result.rows)
                                     }
                                 })
                             }
