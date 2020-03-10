774c81bd1f523d014a3cacc9680ff4b23bbd03bf
hcasanov <noelledeur@gmail.com>
2020-03-10 11:04:22 +0100

    Create locations init

--- config/init.js
+++ config/init.js
@@ -20,6 +20,7 @@ database.query("DROP TABLE IF EXISTS chats;");
 database.query("DROP TABLE IF EXISTS likes;");
 database.query("DROP TABLE IF EXISTS matchs;");
 database.query("DROP TABLE IF EXISTS reports;");
+database.query("DROP TABLE IF EXISTS block;");
 console.log("1 - Tables droped")
 
 setTimeout(init, 2000);
@@ -33,7 +34,7 @@ function ft_exit() {
 function init() {
     database.query("CREATE TABLE IF NOT EXISTS accounts (id SERIAL PRIMARY KEY NOT NULL, login VARCHAR(50), name VARCHAR(50) NOT NULL, firstname VARCHAR(50) NOT NULL, mail VARCHAR(50) NOT NULL, passwd VARCHAR(255) NOT NULL, datebirth VARCHAR(100) NOT NULL, age SMALLINT, gender VARCHAR(10), description TEXT, hashtags TEXT, research_perimeter INT default 20, research_age_min SMALLINT, research_age_max SMALLINT, research_gender VARCHAR(10), research_hashtags TEXT, online BOOLEAN default false, last_connection VARCHAR(200), blocked BOOLEAN default false, date_created VARCHAR(100), date_update VARCHAR(100), token VARCHAR(255) default null, confirm BOOLEAN, token_confirm VARCHAR(100))\;");
     database.query("CREATE TABLE IF NOT EXISTS pictures (id SERIAL PRIMARY KEY NOT NULL, id_account SMALLINT, url_picture TEXT, file_name VARCHAR(100), profile_picture BOOLEAN default false, date_created VARCHAR(100))\;");
-    database.query("CREATE TABLE IF NOT EXISTS locations (id INT NOT NULL, latitude FLOAT NOT NULL, longitude FLOAT NOT NULL, date_created VARCHAR(100), date_update VARCHAR(100))\;");
+    database.query("CREATE TABLE IF NOT EXISTS locations (id INT, latitude FLOAT NOT NULL, longitude FLOAT NOT NULL, date_created VARCHAR(100), date_update VARCHAR(100))\;");
     database.query("CREATE TABLE IF NOT EXISTS notifications (id SERIAL PRIMARY KEY NOT NULL, type VARCHAR(50), like_id INT, chat_id INT, match_id INT, name VARCHAR(150), message TEXT, from_id INT, to_id INT, status BOOLEAN, date_created VARCHAR(100), date_update VARCHAR(100))\;");
     database.query("CREATE TABLE IF NOT EXISTS likes (id SERIAL PRIMARY KEY NOT NULL, from_id INT, to_id INT, date_created VARCHAR(100), status BOOLEAN)\;");
     database.query("CREATE TABLE IF NOT EXISTS matchs (id SERIAL PRIMARY KEY NOT NULL, from_id INT, to_id INT, date_created VARCHAR(100), status BOOLEAN)\;");
@@ -83,6 +84,9 @@ async function create_users() {
 
                 await client.query("INSERT INTO accounts (login, name, firstname, mail, passwd, dateBirth, age, gender, description, hashtags, research_age_min, research_age_max, research_gender, date_created, date_update, confirm, token) VALUES (" + i + ", " + i + ", " + i + ", \'" + i + "@gmail.com\', \'" + password + "\', " + dateBirth + ", " + age + ", \'" + gender + "\', 'Mauris pretium molestie enim, tincidunt volutpat ligula accumsan at. Donec ullamcorper hendrerit dapibus. Integer feugiat in nisi vel maximus. Aliquam erat volutpat. Suspendisse potenti. Ut ultrices maximus lobortis', 'cuise,sport,code,foot', " + research_age_min + ", " + research_age_max + ", \'" + research_gender + "\', " + dateBirth + ", " + dateBirth + ", true, \'" + token + "\')", async (err, res) => {
                 });
+                var query = "INSERT INTO locations (id, latitude, longitude) VALUES (\'" + i + "\', '48.8534', '2.3488')"
+                await client.query(query);
+
                 i--;
                 if (i % 5 === 0)
                     console.log(((i - 500) * -1) / 5 + " % - Creating users")
